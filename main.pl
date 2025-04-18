c6017afe3837a22d25d3fbc6ccc266f87dd8d134
Bahounek Ondrej <xbahou00@merlin.fit.vutbr.cz>
2025-04-18 15:18:04 +0200

    rewrote into IDS

--- main.pl
+++ main.pl
@@ -1,5 +1,4 @@
-:- dynamic seen_cube/1.
-
+% Takes in cube(F,R,B,L,U,D) and prints it out in correct format
 print_cube(cube(
 		[F1,F2,F3,F4,F5,F6,F7,F8,F9],   % Front
 		[R1,R2,R3,R4,R5,R6,R7,R8,R9],   % Right
@@ -19,6 +18,7 @@ print_cube(cube(
 	format('~w~w~w\n',[D7,D8,D9])
 	.
 
+% Reads in input and parses it into my cube struct cube(F,R,B,L,U,D)
 parse_input(Cube) :-
 	read_lines(Lines),    
     split_lines(Lines, S),
@@ -47,16 +47,13 @@ solved_side([]).
 solved_side([_]). 
 solved_side([X,X|T]):- solved_side([X|T]). 
 
-% Check if cube is solved
+% Check if cube is solved (all sides are solved)
 solved_cube(cube(F,R,B,L,U,D)) :-
     maplist(solved_side, [F,R,B,L,U,D]).
 
-% pop from front of the queue
-queue_pop([H|T], H, T).
-
 
 % all the different moves
-moves(['U','u','D','d','R','r','L','l','F','f','M','m','E','e','S','s']).
+moves(['U','u','D','d','R','r','L','l','F','f','M','m','E','e','S','s','B','b']).
 
 % just reverse the path, and apply the rotations to the Cube
 apply_path_to(CubeIn,Path, CubeOut) :-
@@ -69,54 +66,28 @@ apply_moves([Move|T], OldCube, NewCube) :-
     rotate(Move, OldCube, TmpCube),
     apply_moves(T, TmpCube, NewCube).
 
-% % Taks in cube and list of paths, apply all the path rotations seperatly, creatign N cubes
-% apply_path_to_list(CubeIn, Paths, CubesOut) :-
-%     % For each Path in Paths, apply apply_path_to(CubeIn, Path, CubeOut), (fixxing the CubeIn as first argument)
-%     maplist(apply_path_to(CubeIn), Paths, CubesOut).
 
-extend_path(Path, NewPaths) :-
+% Generates all sequences (combinations) of moves of given length(depth)
+generate_move_sequences(0, [[]]).
+generate_move_sequences(Depth, Sequences) :-
+    Depth > 0,
+    PrevDepth is Depth - 1,
+    generate_move_sequences(PrevDepth, PrevSequences),
     moves(Moves),
-    findall([Move|Path], member(Move, Moves), NewPaths).
+    findall([Move|Sequences], (member(Sequences, PrevSequences), member(Move, Moves)), Sequences).
 
-% Try each path, apply it to the cube, and succeed if the resulting cube is solved
-try_paths([], _, _) :- fail.
-try_paths([Path|Rest], CubeIn, SolvedPath) :-
-    apply_path_to(CubeIn, Path, RotatedCube),
-	( seen_cube(RotatedCube) ->
-        try_paths(Rest, CubeIn, SolvedPath)
-    ;   assertz(seen_cube(RotatedCube)),
-        ( solved_cube(RotatedCube) ->
-            SolvedPath = Path
-        ;
-            try_paths(Rest, CubeIn, SolvedPath)
-        )
-    )
-	.
 
-% This BFS case catches the first call, where the are none paths yet
-bfs_loop([], Cube, SolvedPath) :-
-	extend_path([], ExtendedPaths),
-	(	try_paths(ExtendedPaths,Cube,SolvedPath)
-
-    ;
-        append([], ExtendedPaths, NewQueue),
-        bfs_loop(NewQueue, Cube, SolvedPath)
-    ).
-% The main bfs loop, takes in, a queue of paths, and the initial loaded cube
-% Pops the first path from the queue, and generate the new 18 paths
-% Check if any of them are solved cube, if so returns the solved path,
-% otherwise append the new paths to the queue, and recursivley calls it self
-bfs_loop([Path|RestQueue], Cube, SolvedPath) :-
-	% write(Path),
-	% write('\n'),
-
-    % apply_path_to(Cube, Path, NewCube),
-	extend_path(Path, ExtendedPaths),
-	(	try_paths(ExtendedPaths,Cube,SolvedPath)
-
-    ;
-        append(RestQueue, ExtendedPaths, NewQueue),
-        bfs_loop(NewQueue, Cube, SolvedPath)
+% Takes in the initial cube, and max depth
+% For every depth, generate all possible moves orders(Paths)
+% Rotate the initial cube to check all those paths, if any of them is solution, return the path
+% otherwise increase the CurrentDepth
+iterative_deepening_search(Cube, MaxDepth, SolutionPath) :-
+    between(0, MaxDepth, CurrentDepth),
+    generate_move_sequences(CurrentDepth, Paths),
+    member(Path, Paths),
+    apply_path_to(Cube, Path, RotatedCube),
+    ( solved_cube(RotatedCube) ->
+        SolutionPath = Path
     ).
 
 % Takes in Cube and path (in the right order already),
@@ -129,23 +100,15 @@ print_output(Cube,[H|T]):-
 	print_output(RotatedCube,T).
 
 main :- 
+	% load in cube
 	parse_input(Cube),
 
+	% find solution
+	iterative_deepening_search(Cube,10,SolutionPath),
+	
+	% Reverse the solution path to the right order
+    reverse(SolutionPath, OrderedMoves),
 
-    (   solved_cube(Cube) ->
-        % write('Cube is already solved.'), nl,
-        print_cube(Cube)
-    ;
-        bfs_loop([], Cube, SolvedPath)
-        % write('Solved path: '), write(SolvedPath), nl
-    ),
-
-    reverse(SolvedPath, OrderedMoves),
+	% Print the output in correct format
 	print_output(Cube,OrderedMoves)
-	% apply_path_to(Cube,SolvedPath,CubeOut),
-
-	% print_cube(CubeOut)
 	.
-
-
-% TODO testing
